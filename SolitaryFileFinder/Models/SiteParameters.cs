6c32356e31fc67319ab5c49cc82a9d0458a3081a
ShTair <a@vsh.jp>
2017-03-13 16:40:03 +0900

    パラメータのロード

---
+++ SolitaryFileFinder/Models/SiteParameters.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SolitaryFileFinder.Models
+{
+    class SiteParameters
+    {
+        public string Path { get; set; }
+
+        public List<string> RootFiles { get; set; }
+
+        public List<string> DefaultNames { get; set; }
+
+        public List<string> IgnorePaths { get; set; }
+
+        public List<string> CheckPatterns { get; set; }
+
+        public List<string> IgnorePatterns { get; set; }
+
+        public static SiteParameters GenerateInitial()
+        {
+            return new SiteParameters
+            {
+                Path = "",
+                RootFiles = new List<string> { "/index.html" },
+                DefaultNames = new List<string> { "index.html", "index.htm" },
+                IgnorePaths = new List<string> { "/.git/" },
+                CheckPatterns = new List<string> { @".*\.html", @".*\.htm" },
+                IgnorePatterns = new List<string> { @"\.htaccess", @"\.htpasswd" },
+            };
+        }
+
+        public static async Task<SiteParameters> LoadAsync(string path)
+        {
+            try
+            {
+                string json;
+                using (var reader = File.OpenText(path))
+                {
+                    json = await reader.ReadToEndAsync();
+                }
+                return JsonConvert.DeserializeObject<SiteParameters>(json);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static async Task SaveAsync(string path, SiteParameters value)
+        {
+            var json = JsonConvert.SerializeObject(value, Formatting.Indented);
+            using (var writer = File.CreateText(path))
+            {
+                await writer.WriteAsync(json);
+            }
+        }
+    }
+}
--- SolitaryFileFinder/Program.cs
+++ SolitaryFileFinder/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using SolitaryFileFinder.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace SolitaryFileFinder
@@ -10,6 +8,23 @@ namespace SolitaryFileFinder
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0) args = new string[] { null };
+            Run(args[0]).Wait();
+            Console.WriteLine("** Finished");
+            Console.ReadLine();
+        }
+
+        private static async Task Run(string pp)
+        {
+            var p = await SiteParameters.LoadAsync(pp);
+            if (p == null)
+            {
+                p = SiteParameters.GenerateInitial();
+
+                if (pp == null) pp = "param.json";
+                await SiteParameters.SaveAsync(pp, p);
+                return;
+            }
         }
     }
 }
